7b6e4138716fa736286fb3204983159a636377e6
fotanus@gmail.com <Felipe Tanus>
2016-04-09 22:45:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+To run the bot, you need to create a bot and get a key for him. Put the key in a file `token.txt` in the root directory, and run:
+
+```
+bundle
+./ufrgs-ru-bot
+```
---
+++ ufrgs-ru-bot.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'bundler/setup'
+require 'telegram/bot'
+require 'nokogiri'
+require 'rest-client'
+
+token = File.read('token.txt').strip
+
+def getMenu(n)
+  url = "http://www.ufrgs.br/ufrgs/ru"
+  selector = "#ru#{n} > div:nth-child(2)"
+  html = Nokogiri.HTML(RestClient.get(url))
+  textDiv = html.css(selector)
+  textDiv.css("br").each { |n| n.replace("\n") }
+  text = textDiv.text
+
+  if text.empty?
+    "Não tem RU hoje :-(\n http://www.ufrgs.br/ufrgs/ru"
+  else
+    text
+  end
+rescue => e
+  "deu pau! #{e}\n#{e.backtrace}"
+end
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/ru1'
+      bot.api.send_message(chat_id: message.chat.id, text: " #{getMenu(1)}")
+    when '/ru2'
+      bot.api.send_message(chat_id: message.chat.id, text: " #{getMenu(2)}")
+    when '/ru3'
+      bot.api.send_message(chat_id: message.chat.id, text: " #{getMenu(3)}")
+    when '/ru4'
+      bot.api.send_message(chat_id: message.chat.id, text: " #{getMenu(4)}")
+    when '/ru5'
+      bot.api.send_message(chat_id: message.chat.id, text: " #{getMenu(5)}")
+    when '/ru6'
+      bot.api.send_message(chat_id: message.chat.id, text: " #{getMenu(6)}")
+    else
+      bot.api.send_message(chat_id: message.chat.id, text: "\\ru1, \\ru2, ... \\ru6 para ver o menu do RU")
+    end
+  end
+end
+
